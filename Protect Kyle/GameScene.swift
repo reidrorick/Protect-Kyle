cdca1b1289dce8e89c812738d18ab60000440914
Game Shared <GameDesignShared@mac-lab-d25tq030gg7d-9196.local>
2019-05-01 14:49:10 -0500

    May 1, 2019

--- Protect Kyle/GameScene.swift
+++ Protect Kyle/GameScene.swift
@@ -12,7 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     
 
-    
+    //SpriteNodes
     var background=SKSpriteNode(imageNamed: "background")
     var background2=SKSpriteNode(imageNamed: "background")
     var background3=SKSpriteNode(imageNamed: "background")
@@ -26,16 +26,25 @@ class GameScene: SKScene {
     var gun=SKSpriteNode(imageNamed: "Scar")
     var target=SKSpriteNode(imageNamed: "target")
 
-    
+    //CameraNode
     var myCamera:SKCameraNode?
-    
+   
+    //Booleans
     var rightPressed:Bool=false
     var leftPressed:Bool=false
     var mousePressed:Bool=false
+    var upgrade1:Bool=false
     
+    //Int
+    var zombieNumb:Int=0
+    
+    //CGFloats
     var timer:CGFloat=0
     var relativeDist:CGFloat=0
+    var zombieCount:CGFloat=30
+    var zNumb:CGFloat=0
     
+    //
     let zombie=ZombieClass()
     var theView:SKView?
     
@@ -92,9 +101,7 @@ class GameScene: SKScene {
         backgroundLoop.zPosition = 1
         backgroundLoop.position.x = -background.size.width
         
-        addChild(gun)
-        gun.position.y = -background.size.height/2 + gun.size.height/2
-        gun.zPosition = 4
+        
         
         addChild(target)
         target.zPosition = 3.5
@@ -132,17 +139,52 @@ class GameScene: SKScene {
         {
             if x.name != nil
             {
-                if x.name! == "realLifePatrick"
+                if x.name!.contains("Zombie")
                 {
+                    for thisNode in self.children
+                    {
+                        if thisNode.name != nil
+                        {
+                            if thisNode.name! == x.name!
+                            {
+                                
+                                thisNode.removeFromParent()
+                            }//remove
+                        }//!=nil
+                    }//thisnode
                     x.removeFromParent()
-                    
-                    
-                }
+                }//kill zombie
             }//!=nil
         }//for loop
     }//touchdown
     
     func touchMoved(toPoint pos : CGPoint) {
+        if upgrade1==true
+        {
+            for x in nodes(at: pos)
+            {
+                if x.name != nil
+                {
+                    if x.name!.contains("Zombie")
+                    {
+                        for thisNode in self.children
+                        {
+                            if thisNode.name != nil
+                            {
+                                if thisNode.name! == x.name!
+                                {
+                                    
+                                    thisNode.removeFromParent()
+                                }//remove
+                            }//!=nil
+                        }//thisnode
+                        x.removeFromParent()
+                        zombieNumb += 1
+                    }//kill zombie
+                }//!=nil
+            }//for loop
+        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
@@ -218,10 +260,11 @@ class GameScene: SKScene {
         let zombieSize = 20/relativeDist
         relativeDist = -size.height/2 - tempZombie.position.y
         tempZombie.setScale(zombieSize)
-        tempZombie.name="realLifePatrick"
+        tempZombie.name="Zombie\(zNumb)"
         tempZombie.position.x = random(min: -background.size.width/2, max: background.size.width*5 + background.size.width/2)
         tempZombie.position.y = 0
         addChild(tempZombie)
+        
         tempZombie.zPosition = 3
         
         let actions = SKAction.sequence([SKAction.moveTo(y: -size.height/2-tempZombie.size.height, duration: 10),SKAction.removeFromParent()])
@@ -231,34 +274,57 @@ class GameScene: SKScene {
         tempZombie.run(scale)
         
         if tempZombie.position.x >= background.size.width*5 - background.size.width/2
+            {
+                
+                let temp2Zombie = zombie.zombieSprite.copy() as! SKSpriteNode
+                temp2Zombie.position=temp2Zombie.position
+                temp2Zombie.setScale(tempZombie.xScale)
+                temp2Zombie.name="Zombie\(zNumb)"
+                
+                temp2Zombie.zPosition = 3
+                addChild(temp2Zombie)
+                
+                temp2Zombie.position.x = tempZombie.position.x - background.size.width*6
+                let actions2 = SKAction.sequence([SKAction.moveTo(y: -size.height/2-temp2Zombie.size.height, duration: 10),SKAction.removeFromParent()])
+                let scale2 = SKAction.sequence([SKAction.scale(to: 2, duration: 10)])
+                temp2Zombie.run(scale2)
+                temp2Zombie.run(actions2)
+                
+                
+                
+            }//copy zombie
+            tempZombie.name = "Zombie\(zNumb)"
+            zNumb += 1
+        
+        }//spawn zombies
+    func rounds()
+    {
+        var counter:Int=0
+        for node in children
         {
-            
-            let temp2Zombie = zombie.zombieSprite.copy() as! SKSpriteNode
-            temp2Zombie.position=temp2Zombie.position
-            temp2Zombie.setScale(tempZombie.xScale)
-            temp2Zombie.name="realLifePatrick"
-            
-            temp2Zombie.zPosition = 3
-            addChild(temp2Zombie)
-            
-            temp2Zombie.position.x = tempZombie.position.x - background.size.width*6
-            let actions2 = SKAction.sequence([SKAction.moveTo(y: -size.height/2-temp2Zombie.size.height, duration: 10),SKAction.removeFromParent()])
-            let scale2 = SKAction.sequence([SKAction.scale(to: 2, duration: 10)])
-            temp2Zombie.run(scale2)
-            temp2Zombie.run(actions2)
-            
-            
-            
+            if node.name != nil
+            {
+                if node.name!.contains("Zombie")
+                {
+                    counter += 1
+                }
+            }
+        }//zombies on screen counter
+        
+        
+        if zNumb < zombieCount && timer ==  0
+        {
+            spawnZombies()
+            print(zNumb)
         }
-        
-            
-            
-        
-        
+        else if zNumb == zombieCount
+        {
+            zombieCount = 1.5*zombieCount
+            timer += 15
         }
+    }//rounds()
+            
     
-            
-        
         
         
     
@@ -269,14 +335,8 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        timer += 1/60
-        moveCamera()
-        if timer > 0
-        {
-            spawnZombies()
-            timer = 0
-        }
-    
+            moveCamera()
+            rounds()
         // Called before each frame is rendered
     }
 }
